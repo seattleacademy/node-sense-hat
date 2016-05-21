d9886dc84559f2d8ef5dbb9372d031eacace44e5
Gary Anderson <ganderson@seattleacademy.org>
2016-05-21 14:04:46 -0700

    add python-shell module and test

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 var nodeimu = require('nodeimu');
 var IMU = new nodeimu.IMU();
 
-IMU.getValue(function(err, data) {
-    if (err) console.log(err)
-    else console.log(data);
+var PythonShell = require('python-shell');
+
+PythonShell.run('python/helloworld.py', function (err, results) {
+  if (err) throw err;
 });
+
+IMU.getValue(function(err, data) {
+    if (err) throw err;
+    console.log(data);
+});
+
